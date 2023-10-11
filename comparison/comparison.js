4affc934d8f3535d35945bfa2ad8847f7203dfde
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 20:34:15 +0600

    Comparison

---
+++ comparison/comparison.js
@@ -0,0 +1,27 @@
+// console.log(10 < 11);
+// console.log(10 < 9);
+// // ----------------------
+// console.log(5 == 6);
+// console.log(6 == 6);
+// // ----------------------
+// console.log(5 != 6);
+// console.log(6 != 6);
+// // ----------------------
+// console.log(5 <= 6);
+// console.log(10 <= 5);
+// // ----------------------
+// console.log(5 >= 6);
+// console.log(6 >= 6);
+// -------------------------------------
+var num1 = 56;
+var num2 = 60;
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// --------------------------------------
+// Multiple Condition--------------------
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 // result1 // result2 // height1 > height2
