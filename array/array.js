93cb43ceed2ae94e8696f01051b24671987771ab
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 19:59:16 +0600

    Index

--- array/array.js
+++ array/array.js
@@ -1,4 +1,10 @@
 // array declaration
-var friendName = ['Mehedi', 'Ohin', 'Mufassir', 'Omar', 'Arman'];
+var friendName = ['Mehedi', 'Ohin', 'Mufassir', 'Omar'];
 var tourFee = [500, 300, 600, 1000, 200];
+
+// console.log(friendName);
+// console.log(tourFee);
+
+console.log(friendName.length);
+console.log(tourFee.length);
 
---
+++ index/index.js
@@ -0,0 +1,14 @@
+var run = [200, 60, 34, 50, 55];
+ 
+// get element by index.
+var element = run[1]
+// console.log(element);
+
+// set element new index by value
+run[1] = 70;
+run[3] = 45;
+// console.log(run);
+
+// find index of an element
+var position = run.indexOf(55);
+// console.log(position);
