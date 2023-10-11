b8834d1d4a7ad268ea69a3511e8007356817a052
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 20:49:16 +0600

    conditional

---
+++ conditional/conditional.js
@@ -0,0 +1,28 @@
+var iPhone = 80000;
+var myBudget = 500000;
+// if iPhone price is less than my budget than i will buy the iPhone
+
+if(iPhone < myBudget){
+    // console.log('I will buy the phone');
+}
+else{
+    // console.log('You do not buy this phone');
+}
+
+// --------------------------------------
+var chickenPrice = 500;
+var myMoney = 50;
+
+if(chickenPrice <= myMoney){
+    // console.log('Chicken Purchage');
+}
+if(chickenPrice > myMoney){
+    // console.log('go to potato store');
+}
+// --------------------------------------
+if(chickenPrice <= myMoney){
+    console.log('I will Eat Chickem');
+}
+else{
+    console.log('I will Eat Potato');
+}
