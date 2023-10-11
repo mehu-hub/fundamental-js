94ca3f8acbed77307bf0597dcebb2140f517be08
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 21:43:10 +0600

    multistage-condition

---
+++ multistage-condition/multistage-condition.js
@@ -0,0 +1,17 @@
+var myMoney = 50;
+var danis = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+// -------------------------------
+if (danis < myMoney) {
+    console.log('Purchage Danish');
+}
+else if (butterBread < myMoney) {
+    console.log('Purchage Butter');
+}
+else if (toastBiscuit < myMoney) {
+    console.log('Purchage Biscuite');
+}
+else{
+    console.log('Cha kha vai');
+}
