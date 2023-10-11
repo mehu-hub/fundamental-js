51ed67065b504eca66d735b9017c58dc85a1c8a9
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 20:00:33 +0600

    Array

--- array/array.js
+++ array/array.js
@@ -1,6 +1,6 @@
 // array declaration
 var friendName = ['Mehedi', 'Ohin', 'Mufassir', 'Omar'];
-var tourFee = [500, 300, 600, 1000, 200];
+var tourFee = [50, 300, 600, 1000, 200];
 
 // console.log(friendName);
 // console.log(tourFee);
