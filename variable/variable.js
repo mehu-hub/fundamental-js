161bf26c3d800603b3f3eae8eadd6a54af830083
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 19:31:27 +0600

    first commit

---
+++ variable/variable.js
@@ -0,0 +1,32 @@
+// number type variable 
+var price = 25;
+
+// string type variable
+var name = 'Mehedi Hasan';
+
+// boolean type variable
+var isHappy = false;
+// -------------------------
+/*
+---two types of number
+* Integer - 120;
+* Float, decimal - 12.5;
+*/ 
+// ---------------------------
+// variable naming convention: rules, best-practice
+/**
+ * Single word
+ * no gap or space
+ * no quote
+ * no keyword
+ * can not start with a number
+ * can not contain dash 
+ * should use camelCase
+*/ 
+// --------------------------
+// math operation: +, -, *, /,
+// Short Hand: +=, -=, *=, /=,
+// --------------------------
+// covert: parseInt, parseFolat
+// toFixed
+// reminder: vaghshes: %
