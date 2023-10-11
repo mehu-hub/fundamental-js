ababfd1aa35d0f9a4211ccc038c19aa00e18c04c
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 20:17:27 +0600

    Push And Pop

---
+++ push-pop/push-pop.js
@@ -0,0 +1,26 @@
+var numbers = [78, 45, 50, 90];
+// use push to add element to an array of last index
+numbers.push(100);
+// console.log(numbers);
+
+var friends = ['Mehedi', 'Ohin', 'Mufassir'];
+friends.push('Omar');
+// console.log(friends);
+
+// console.log(numbers);
+
+// use pop to get last element
+numbers.pop();
+var newNumbers = numbers.pop();
+// console.log(numbers);
+// console.log(newNumbers);
+
+// ---------------------------------------------
+
+var bikes = ['Yamaha', 'Suzuki', 'Royal'];
+// Add array item in first element
+bikes.unshift('Honda');
+// remove array item in first element
+bikes.shift()
+console.log(bikes);
+
