94e82b68950f91f54c64998c75b87fea20856682
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 21:33:48 +0600

    Multiple Condition

---
+++ multiple-condition/multiple-condition.js
@@ -0,0 +1,40 @@
+// --------------------------------------
+// if(isGraduated === true){
+//     console.log('Aso Biya kori !!!');
+// }
+// else{
+//     console.log('Age Graduated Houu !!');
+// }
+// ---------------------------------------
+// if(isGraduated == true && salary > 50000 ){
+//     console.log('Lets Get Married');
+// }
+// else{
+//     console.log('Salary Baraw');
+// }
+//----------------------------------------
+// if(isGraduated == true && salary > 50000 && cars >= 1 ){
+//     console.log('Lets Get Married');
+// }
+// else{
+//     console.log('Salary Baraw');
+// }
+//----------------------------------------
+var isGraduated = true;
+var salary = 45000;
+var cars = 0;
+// ---------------------------------------
+// if(isGraduated == true || salary > 50000 || cars >= 1){
+//     console.log('Let Jump to Love');
+// }
+// else{
+//     console.log('You are poor');
+// }
+// ---------------------------------------
+// if ((isGraduated == true && salary > 50000) || cars >= 1) {
+//     console.log('Go to the Kazi Office');
+// }
+// else{
+//     console.log('Salary Baraw');
+// }
+
