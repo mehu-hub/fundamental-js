5c7be89a2fc93c1c3101ae3a3aa4ff155668fd67
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 21:46:23 +0600

    nested-condition

---
+++ nested-condition/nested-condition.js
@@ -0,0 +1,17 @@
+var math = true;
+var geometry = true;
+var strateline = false;
+
+if(math === true){
+    if(geometry === true){
+        if(strateline == true){
+
+        }
+        else{
+            console.log('Baka pothe Colba na');
+        }
+    }
+    else{
+        console.log('Pithagoras Hoite Parba na !!');
+    }
+}
